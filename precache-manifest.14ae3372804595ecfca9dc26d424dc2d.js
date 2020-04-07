84f4176c106078ab5bdfc9d0c49693fcd88dca02
MeRichard123 <rcoric03@gmail.com>
2020-04-07 14:29:44 +0100

    Updates

---
+++ precache-manifest.14ae3372804595ecfca9dc26d424dc2d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a273ea910ecce49bc83edb8310ba0969",
+    "url": "/numword/index.html"
+  },
+  {
+    "revision": "5d643835ac9beb4a3d29",
+    "url": "/numword/static/css/main.77a861c6.chunk.css"
+  },
+  {
+    "revision": "01cd7d8fc9459df770e1",
+    "url": "/numword/static/js/2.25c56cb7.chunk.js"
+  },
+  {
+    "revision": "ecc441d2f9cabc7ffffd11bd27c959ab",
+    "url": "/numword/static/js/2.25c56cb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d643835ac9beb4a3d29",
+    "url": "/numword/static/js/main.a240ac7d.chunk.js"
+  },
+  {
+    "revision": "467af5da707d475679e3",
+    "url": "/numword/static/js/runtime-main.952b733a.js"
+  }
+]);
